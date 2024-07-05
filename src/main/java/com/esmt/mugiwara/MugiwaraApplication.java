7941b9855c401c513a080727e9ca9a8de3c9d2c3
diahabib <habibdia45@gmail.com>
2024-07-05 16:50:00 +0000

    First Commit

---
+++ src/main/java/com/esmt/mugiwara/MugiwaraApplication.java
@@ -0,0 +1,13 @@
+package com.esmt.mugiwara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugiwaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugiwaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esmt/mugiwara/MugiwaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmt.mugiwara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugiwaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
